86ef996289a1511783523a9558bf0c6d1dd630e8
JohnJaimeSeba <COMLAB507@DESKTOP-DBFKH07>
2025-04-23 12:27:05 +0800

    sample ni sir erwin

---
+++ Components/Pages/Messages.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuApp.Components.Pages
+{
+    public partial class Messages:ComponentBase
+    {
+        public string Message { get; set; }
+        protected async void Initialized()
+        {
+            var messageFilePath = FileSystem.AppDataDirectory + "/messages.txt";
+            if (!File.Exists(messageFilePath))
+            {
+                File.WriteAllText(messageFilePath, "Hello World");
+            }
+
+            //var readdata = File.ReadAllText(messageFilePath);
+            Message = "Hello World";
+        }
+    }
+}
---
+++ Extensions/Extensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuApp.Extensions
+{
+    /// <summary>
+    /// Meant for MAUI Blazor Only
+    /// This applies a custom extension to the BlazorWebView to load the AppData directory virtual within the Blazor App Server 0.0.0.0
+    /// DO NOT Create folders in wwwroot with the same name as intended folders to your AppData Directory
+    /// </summary>
+    public class CustomFilesBlazorWebView : BlazorWebView
+    {
+        public override IFileProvider CreateFileProvider(string contentRootDir)
+        {
+            var lPhysicalFiles = new PhysicalFileProvider(FileSystem.Current.AppDataDirectory);
+            return new CompositeFileProvider(lPhysicalFiles, base.CreateFileProvider(contentRootDir));
+        }
+    }
+}
