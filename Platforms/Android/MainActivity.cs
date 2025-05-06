3c50491b5ac95376a0de5b5072454d22fb649d45
RZ Roldan <rickyzroldan@gmail.com>
2025-05-06 13:59:09 +0800

    App Icon/ Splash/ Status Bar

--- Platforms/Android/MainActivity.cs
+++ Platforms/Android/MainActivity.cs
@@ -1,11 +1,26 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace MenuApp
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density, ScreenOrientation = ScreenOrientation.Landscape)]
     public class MainActivity : MauiAppCompatActivity
     {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Change the status bar color //Green
+            Window.SetStatusBarColor(Android.Graphics.Color.ParseColor("#1f5514")); // Replace with desired color
+
+
+            // Make the status bar transparent
+            //Window.SetFlags(WindowManagerFlags.LayoutNoLimits, WindowManagerFlags.LayoutNoLimits);
+            //Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+
+        }
     }
 }
